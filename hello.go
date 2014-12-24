ffa3284ecc612e1d03dddbe10d8afc2d056e1032
lparis <lparis@apcera.com>
2014-12-23 18:21:47 -0800

    second commit

--- hello.go
+++ hello.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/lparis/stringutil"
+)	
+
+//func main() {
+//	fmt.Printf("Hello, world.\n")
+//}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
-}
+	fmt.Printf("Hello, San Francisco?\n")
+	fmt.Printf("Hello, California.\n")
+	fmt.Printf("Hello, wrldsrf\n")
+	fmt.Printf("Hello, World.\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Printf("\n")
+}	
+
+
